8c80635c3f2d9ada31ecc8b7bf998ab0c99868e3
Milos <58392839+rakankoo@users.noreply.github.com>
2021-12-21 18:12:48 +0100

    Add files via upload

---
+++ tabelaArtikal.sql
@@ -0,0 +1,14 @@
+use stavka
+
+create table artikal(
+id int identity (1,1),
+kom int,
+cena int,
+popust int)
+
+insert into artikal values
+(123,55000,60),
+(500,44000,20),
+(1000,320,0),
+(10,31210,50)
+
